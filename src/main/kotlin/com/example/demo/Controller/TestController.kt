fa04f30f517d4e3ae5fbc5467362a1f790a529e9
raymond yap <raymond_yap@trendmicro.com>
2022-09-15 22:06:39 +0800

    add other new line

--- src/main/kotlin/com/example/demo/Controller/TestController.kt
+++ src/main/kotlin/com/example/demo/Controller/TestController.kt
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
+
 @RestController
 class TestController {
 
